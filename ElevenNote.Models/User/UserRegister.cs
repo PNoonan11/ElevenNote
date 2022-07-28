900355415b30b0e188e39f8702e75ec0df832e26
Patrick Noonan <Prnoonan11@gmail.com>
2022-07-27 19:24:11 -0500

    password hasher established for user registration

---
+++ ElevenNote.Models/User/UserRegister.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Models.User
+{
+    public class UserRegister
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string Username { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string Password { get; set; }
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- ElevenNote.Services/User/IUserService.cs
+++ ElevenNote.Services/User/IUserService.cs
@@ -1,7 +1,10 @@
+using ElevenNote.Models.User;
+using System.Threading.Tasks;
+
 namespace ElevenNote.Services.User
 {
     public interface IUserService
     {
-         
+        Task<bool> RegisterUserAsync(UserRegister model);
     }
 }
