9f44268b23b25cb88f60aa52febfb77a0108481e
Patrick Noonan <Prnoonan11@gmail.com>
2022-08-02 20:32:19 -0500

    CategortEntity scaffolded

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ namespace ElevenNote.Data
 
         public DbSet<UserEntity> Users { get; set; }
         public DbSet<NoteEntity> Notes { get; set; }
+        public DbSet<CategoryEntity> Category { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ ElevenNote.Data/Entities/CategoryEntity.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Data.Entities
+{
+    public class CategoryEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
